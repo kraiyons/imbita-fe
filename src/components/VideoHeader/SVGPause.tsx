c7726909ed077f2cbbee2a13060f8b68e3a24def
Krai Alapide <krai.alapide@gmail.com>
2023-07-12 01:16:25 +0800

    [Chore] Fix build error

--- src/components/VideoHeader/SVGPause.tsx
+++ src/components/VideoHeader/SVGPause.tsx
@@ -6,10 +6,9 @@ const SVGPause = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-pause-circle"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <circle cx="12" cy="12" r="10" />
     <line x1="10" y1="15" x2="10" y2="9" />
