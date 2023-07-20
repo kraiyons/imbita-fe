4c08cfc89798dc39f85c4db84d206b952c9e236c
Krai Alapide <krai.alapide@gmail.com>
2023-07-21 00:19:10 +0800

    [WIP] Mute button click event
    Need to fix z index and position, polish the click events bubbling to avoid conflict to pause event

--- src/components/VideoHeader/index.tsx
+++ src/components/VideoHeader/index.tsx
@@ -8,27 +8,38 @@ type TProps = {
   media: string;
 };
 
-const VolumeSVG = (isMuted: boolean) => {
-  return <>{isMuted ? <SVGMute /> : <SVGVolume />}</>;
-};
-
 export default function VideoHeader({ media }: TProps) {
-  const [state, setState] = useState(true);
+  const [state, setState] = useState({ isPlaying: true, isMuted: true });
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  const _handleVideoClick = () => {
+  const _handleVideoClick = (e: MouseEvent<HTMLDivElement>) => {
+    e.current.preventBubble();
     if (!videoRef.current) return;
     const isPlaying = !videoRef.current.paused;
     isPlaying ? videoRef.current.pause() : videoRef.current.play();
-    setState(!isPlaying);
+    setState((pState) => ({ ...pState, isPlaying }));
   };
+
+  const _handleUnmute = (e) => {
+    e.preventBubble();
+    if (!videoRef.current) return;
+    const isMuted = !videoRef.current.muted;
+    videoRef.current.muted = isMuted;
+    setState((pState) => ({ ...pState, isMuted }));
+  };
+
   return (
-    <div className="flex items-center justify-center mb-10 -rotate-2 will-change-transform">
+    <div className="flex items-center justify-center mb-10 -rotate-2 will-change-transform relative">
+      <div
+        onClick={_handleUnmute}
+        className="absolute bottom-0 right-0 z-20 border"
+      >
+        {videoRef.current?.muted ? <SVGMute /> : <SVGVolume />}
+      </div>
       <div
         className="relative z-10 h-auto w-[300px] shadow-2xl rounded-lg bg-white p-4 overflow-hidden"
         onClick={_handleVideoClick}
       >
-        <VolumeSVG isMuted={false} />
         {!state && (
           <div className="bg-gray-700/20 w-full h-full text-white absolute top-0 left-0">
             <div className="absolute top-[calc(50%-12px)] left-[calc(50%-12px)]">
